98a10e9ee20f62e2606158bcdf31585b12dc7cc2
Durmuş PALA <71263028+durmus-pala@users.noreply.github.com>
2021-05-02 14:20:37 +0300

    fix da/night switch condition

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-constant-condition */
+/* eslint-disable linebreak-style */
+/* eslint-disable no-cond-assign */
+/* eslint-disable linebreak-style */
 const mySwitch = document.getElementById('check');
 mySwitch.addEventListener('click', () => {
-    if(mySwitch.checked = true){
-        document.body.style.backgroundColor = 'black';
-        document.body.style.color = 'white';
-    }
-    else if(mySwitch.checked = false){
-        document.body.style.backgroundColor = '#f4f4f4';
-        document.body.style.color = 'black';
-    }
+  if (mySwitch.checked === true) {
+    document.body.style.backgroundColor = 'black';
+    document.body.style.color = 'white';
+  } else if (mySwitch.checked === false) {
+    document.body.style.backgroundColor = '#f4f4f4';
+    document.body.style.color = 'black';
+  }
 });
